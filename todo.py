e4376407a95a02e33f8b005a1c33a45015addfd2
Rahul Nakre <rahulnakre@gmail.com>
2018-05-04 22:19:21 -0400

    added fucntionality to add to and view the todo list, added the cool '***', made the program prompt user for inout forever

--- todo.py
+++ todo.py
@@ -1,10 +1,18 @@
 def run() -> None:
     '''The main module that prompts the user for what they wana do
     '''
+    print('Menu')
     print("1) View Todo")
     print("2) Add Task")
-    print("1) View Todo")
     i = input("What would you like to do?: ")
+
+
+    if i == '1':
+        print(t)
+    elif i == '2':
+        t.add()
+
+    run()
 
 
 def case_generator(s: str) -> 'List[str]':
@@ -26,11 +34,15 @@ def case_generator(s: str) -> 'List[str]':
 
 class Todo:
 
-    def __init__(self: "Todo", size: int, data: dict={}):
+    def __init__(self: "Todo", size: int=0, data: dict={}):
         self.size, self.data = size, data
 
     def __repr__(self) -> str:
-        s = ''
+        if self.size == 0:
+            return '**************************\nEmpty Todo list' + '\n**************************\n'
+
+        s = '**************************\n'
+        s += 'TODO LIST: \n'
         count = 0
         for i in self.data:
             if count == (self.size - 1):
@@ -38,8 +50,14 @@ class Todo:
             else:
                 s += str(i) + ")" + " " + self.data[i] + '\n'
             count += 1
-        return s
+        return s + '\n**************************\n'
+
+    def add(self) -> None:
+        task = input("Enter Task: ")
+        self.size += 1
+        self.data[self.size] = task
 
 
 if __name__ == '__main__':
+    t = Todo()
     run()
